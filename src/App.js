f997ccec4fde631d4afa561611ca810a8d329c34
yulia <nappestok@gmail.com>
2022-05-14 19:00:00 +0200

    categories list added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import './categories.styles.scss'
+
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    }
+  ]
+
   return (
-    <div>Hello</div>
+    <div className="categories-container">
+      {categories.map(({id, title}) => (
+        <div className="category-container" key={id}>
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      )
+      )}
+    </div>
   );
 }
 
