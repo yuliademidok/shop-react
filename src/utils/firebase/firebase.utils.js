737e70d1e49013c9814b4da0441b633f4e1b3d79
yulia <nappestok@gmail.com>
2022-05-16 16:51:46 +0200

    added google sign in component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -35,11 +35,23 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, 'users', userAuth.uid);
-  
-  console.log(userDocRef);
-
   const userSnapshot = await getDoc(userDocRef);
 
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    }
+    catch (error) {
+      console.error('error createing a user', error.message);
+    }
+  }
+
+  return userDocRef;
 }
