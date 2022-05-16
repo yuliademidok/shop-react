77cc2b5b3f23f11275fa145edfb5bc5b9e3e85b5
yulia <nappestok@gmail.com>
2022-05-16 08:51:04 +0200

    added main page nav with logo, shop to navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router';
 
 import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+
 
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </ Route>
     </Routes>
   );
 }
