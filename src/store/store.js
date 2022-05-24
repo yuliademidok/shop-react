e16571f4ebff5c1244d0906dc2035c4c1301d18a
yulia <nappestok@gmail.com>
2022-05-24 22:30:15 +0200

    added redux-persist, added cart and products to the storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 // import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
@@ -7,17 +9,26 @@ const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action);
   }
-  console.log('type', action.type);
-  console.log('payload', action.payload);
-  console.log('currentState', store.getState());
+  console.log('type:', action.type);
+  console.log('payload:', action.payload);
+  console.log('currentState:', store.getState());
 
   next(action);
-  console.log('next state', store.getState())
+  console.log('next state:', store.getState())
 }
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [loggerMiddleware];
 // const middleWares = [logger];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store);
